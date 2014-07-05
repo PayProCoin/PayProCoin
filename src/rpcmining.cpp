33d765d40b1df283973c31e1d39e8bcacd13adcd
becausepool <becausepool@gmail.com>
2014-07-05 16:53:15 -0400

    working nethash

--- src/rpcmining.cpp
+++ src/rpcmining.cpp
@@ -60,62 +60,53 @@ Value gethashespersec(const Array& params, bool fHelp)
     return (boost::int64_t)dHashesPerSec;
 }
 Value GetNetworkHashPS(int lookup, int height) {
-    CBlockIndex *pb = chainActive.Tip();
-
-    if (height >= 0 && height < chainActive.Height())
-        pb = chainActive[height];
-
-    if (pb == NULL || !pb->nHeight)
-        return 0;
-
-    // If lookup is -1, then use blocks since last difficulty change.
-    if (lookup <= 0)
-        lookup = pb->nHeight % 2016 + 1;
-
-    // If lookup is larger than chain, then set it to chain length.
-    if (lookup > pb->nHeight)
-        lookup = pb->nHeight;
-
-    CBlockIndex *pb0 = pb;
-    int64_t minTime = pb0->GetBlockTime();
-    int64_t maxTime = minTime;
-    for (int i = 0; i < lookup; i++) {
-        pb0 = pb0->pprev;
-        int64_t time = pb0->GetBlockTime();
-        minTime = std::min(time, minTime);
-        maxTime = std::max(time, maxTime);
-    }
-
-    // In case there's a situation where minTime == maxTime, we don't want a divide by zero exception.
-    if (minTime == maxTime)
-        return 0;
-
-    uint256 workDiff = pb->nChainWork - pb0->nChainWork;
-    int64_t timeDiff = maxTime - minTime;
-
-    return (int64_t)(workDiff.getdouble() / timeDiff);
-}
-
-Value getnetworkhashps(const Array& params, bool fHelp)
-{
-    if (fHelp || params.size() > 2)
-        throw runtime_error(
-            "getnetworkhashps ( blocks height )\n"
-            "\nReturns the estimated network hashes per second based on the last n blocks.\n"
-            "Pass in [blocks] to override # of blocks, -1 specifies since last difficulty change.\n"
-            "Pass in [height] to estimate the network speed at the time when a certain block was found.\n"
-            "\nArguments:\n"
-            "1. blocks     (numeric, optional, default=120) The number of blocks, or -1 for blocks since last difficulty change.\n"
-            "2. height     (numeric, optional, default=-1) To estimate at the time of the given height.\n"
-            "\nResult:\n"
-            "x             (numeric) Hashes per second estimated\n"
-            "\nExamples:\n"
-            + HelpExampleCli("getnetworkhashps", "")
-            + HelpExampleRpc("getnetworkhashps", "")
-       );
-
-    return GetNetworkHashPS(params.size() > 0 ? params[0].get_int() : 120, params.size() > 1 ? params[1].get_int() : -1);
-}
+     CBlockIndex *pb = pindexBest;
+ 
+     if (height >= 0 && height < nBestHeight)
+         pb = FindBlockByHeight(height);
+ 
+     if (pb == NULL || !pb->nHeight)
+         return 0;
+ 
+     // If lookup is -1, then use blocks since last difficulty change.
+     if (lookup <= 0)
+         lookup = pb->nHeight % 2016 + 1;
+ 
+     // If lookup is larger than chain, then set it to chain length.
+     if (lookup > pb->nHeight)
+         lookup = pb->nHeight;
+ 
+     CBlockIndex *pb0 = pb;
+     int64 minTime = pb0->GetBlockTime();
+     int64 maxTime = minTime;
+     for (int i = 0; i < lookup; i++) {
+         pb0 = pb0->pprev;
+         int64 time = pb0->GetBlockTime();
+         minTime = std::min(time, minTime);
+         maxTime = std::max(time, maxTime);
+     }
+ 
+     // In case there's a situation where minTime == maxTime, we don't want a divide by zero exception.
+     if (minTime == maxTime)
+         return 0;
+ 
+     uint256 workDiff = pb->nChainWork - pb0->nChainWork;
+     int64 timeDiff = maxTime - minTime;
+ 
+     return (boost::int64_t)(workDiff.getdouble() / timeDiff);
+ }
+ 
+ Value getnetworkhashps(const Array& params, bool fHelp)
+ {
+     if (fHelp || params.size() > 2)
+         throw runtime_error(
+             "getnetworkhashps [blocks] [height]\n"
+             "Returns the estimated network hashes per second based on the last 120 blocks.\n"
+             "Pass in [blocks] to override # of blocks, -1 specifies since last difficulty change.\n"
+             "Pass in [height] to estimate the network speed at the time when a certain block was found.");
+ 
+     return GetNetworkHashPS(params.size() > 0 ? params[0].get_int() : 120, params.size() > 1 ? params[1].get_int() : -1);
+ }
 
 Value getmininginfo(const Array& params, bool fHelp)
 {
